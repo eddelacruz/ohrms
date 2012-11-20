887da4628565472d202dc3e76b7dcbc2e28351f8
eddelacruz <elidc93@gmail.com>
2012-11-21 01:50:57 +0800

    pdf module

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.mvc.Result
+import util.pdf.PDF
+import views._
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(html.index("Your new application is ready."))
   }
-  
+
+  def samplePdf(id: String): Result = {
+    return PDF.ok(html.samplePdf.render)
+  }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "mysql" % "mysql-connector-java" % "5.1.18"
+      "mysql" % "mysql-connector-java" % "5.1.18",
+      "pdf" % "pdf_2.9.1" % "0.3"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      resolvers += Resolver.url("My GitHub Play Repository", url("http://www.joergviola.de/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
