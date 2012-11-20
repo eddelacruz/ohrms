5c99684df9815180ac5e65eb080e92744c8cff6e
bjsmallari <billyjoelmallari@gmail.com>
2012-11-20 14:09:40 +0800

    databse connection

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "mysql" % "mysql-connector-java" % "5.1.18"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
