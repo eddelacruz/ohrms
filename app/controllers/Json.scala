8dcf999421e7157a532f5427fb4e9dfe0101659d
unknown <bernardojohanakirstie@gmail.com>
2012-12-19 05:03:38 +0800

    staff na wla pang update at add at delete

--- app/controllers/Json.scala
+++ app/controllers/Json.scala
@@ -5,15 +5,14 @@ import libs.json.JsObject
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.json.Json._
-import views.html.{patient, modal}
 import ws.services._
 import ws.generator.UUIDGenerator
 import ws.helper.WsHelper
-import ws.deserializer.json.{AuditLogDeserializer, PatientListDeserializer, DentistListDeserializer, DentalServiceListDeserializer}
+import ws.deserializer.json.{AuditLogDeserializer, PatientListDeserializer, DentistListDeserializer, DentalServiceListDeserializer, StaffListDeserializer}
 import collection.mutable.ListBuffer
 import ws.services.PatientList
 import ws.services.DentistList
-import ws.services.DentalServiceList
+import ws.services.StaffList
 
 
 /**
@@ -23,7 +22,7 @@ import ws.services.DentalServiceList
  * Time: 12:41 PM
  * To change this template use File | Settings | File Templates.
  */
-object Json extends Controller with WsHelper with PatientListDeserializer with AuditLogDeserializer with DentistListDeserializer with DentalServiceListDeserializer{
+object Json extends Controller with WsHelper with PatientListDeserializer with AuditLogDeserializer with DentistListDeserializer with DentalServiceListDeserializer with StaffListDeserializer{
 
   def getPatientList(start: Int, count: Int) = Action {
     Ok(JsObject(Seq("PatientList" -> toJson(PatientService.getPatientList(start, count)))))
@@ -35,6 +34,14 @@ object Json extends Controller with WsHelper with PatientListDeserializer with A
 
   def getDentalServiceList(start: Int, count: Int) = Action {
     Ok(JsObject(Seq("DentalServiceList" -> toJson(ServicesService.getDentalServiceList(start, count)))))
+  }
+
+  def getStaffList(start: Int, count: Int) = Action {
+    Ok(JsObject(Seq("StaffList" -> toJson(StaffService.getStaffList(start, count)))))
+}
+
+  def getStaffById(id : String) = Action {
+    Ok(JsObject(Seq("StaffList" -> toJson(StaffService.getStaffListById(id)))))
   }
 
   def getDentalServiceInformationById(id : String) = Action {
@@ -262,5 +269,53 @@ object Json extends Controller with WsHelper with PatientListDeserializer with A
         Status(500)
       }
   }
+
+
+  def submitStaffAddForm = Action {
+    implicit request =>
+      val id = ""
+      val firstName = request.body.asFormUrlEncoded.get("first_name").head
+      val middleName = request.body.asFormUrlEncoded.get("middle_name").head
+      val lastName = request.body.asFormUrlEncoded.get("last_name").head
+      val contactNo = request.body.asFormUrlEncoded.get("contact_no").head
+      val address = request.body.asFormUrlEncoded.get("address").head
+      val position = request.body.asFormUrlEncoded.get("position").head
+      val userName = request.body.asFormUrlEncoded.get("user_name").head
+      val password = request.body.asFormUrlEncoded.get("password").head
+      val s = StaffList("", firstName, middleName, lastName, contactNo, address, position, userName, password)
+
+      if (StaffService.addStaff(s) >= 1) {
+        Redirect("/staffs")
+        Status(200)
+      } else {
+        BadRequest
+        Status(500)
+      }
+
+  }
+
+
+  def submitStaffUpdateForm = Action {
+    implicit request =>
+      val id =  request.body.asFormUrlEncoded.get("id").head
+      val firstName = request.body.asFormUrlEncoded.get("first_name").head
+      val middleName = request.body.asFormUrlEncoded.get("middle_name").head
+      val lastName = request.body.asFormUrlEncoded.get("last_name").head
+      val contactNo = request.body.asFormUrlEncoded.get("contact").head
+      val address = request.body.asFormUrlEncoded.get("address").head
+      val position = request.body.asFormUrlEncoded.get("position").head
+      val userName = request.body.asFormUrlEncoded.get("user_name").head
+      val password = request.body.asFormUrlEncoded.get("password").head
+      val s = StaffList(id, firstName, middleName, lastName, contactNo, address, position, userName, "")
+
+      if (StaffService.updateStaff(s) >= 1) {
+        Status(200)
+      } else {
+        BadRequest
+        Status(500)
+      }
+  }
+
+
 
 }
